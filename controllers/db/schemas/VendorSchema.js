afe636d99042ef6e1ef3d22dfd6ca90b5677e7fc
farris.ismati@gmail.com <farris.ismati@gmail.com>
2018-08-20 22:04:29 -0400

    Expanded VendorSchema

--- controllers/db/schemas/VendorSchema.js
+++ controllers/db/schemas/VendorSchema.js
@@ -4,35 +4,74 @@ const mongoose     = require('../connection.js')
 //VENDOR SCHEMA
 const VendorSchema = new mongoose.Schema({
   name : { type: String, required: true },
-  type: { type: String, required: true },
-  establishedDate: { type: Date, required: true },
-  closedDate: { type: Date, required: false },
-  description: { type: String, required: true },
-  //menu
-  facebookRating: { type: String, required: false },
-  yelpRating: { type: String, required: false },
-  twitterID: { type: String, required: true },
-  //tweets
-  //coordinates
-  //coordinatesHistory
-  dailyActive: { type: Boolean, required: true },
-  address: { type: String, required: false },
-  consecutiveDaysInactive: { type: Number, required: false },
-  //categories
-  price: { type: String, required: true },
-  //regionID
-  objCreatedAt: { type: Date, default: Date.now }
+  type : { type: String, required: true, enum: ['mobileTruck', 'fixedTruck', 'cart', 'airstream'] },
+  establishedDate : { type: Date, required: true },
+  closedDate : { type: Date, required: false },
+  description : { type: String, required: true },
+  menu : {
+    type: [MenuSchema],
+    required: true
+  },
+  facebookRating : { type: String, required: false },
+  yelpRating : { type: String, required: false },
+  twitterID : { type: String, required: true },
+  tweets : {
+    type: [TweetSchema],
+    required: false
+  },
+  coordinates : {
+    type: CoordinatesSchema,
+    required: false
+  },
+  coordinatesHistory : {
+    type: [CoordinatesSchema],
+    required: false
+  },
+  dailyActive : { type: Boolean, required: true },
+  address : { type: String, required: false },
+  consecutiveDaysInactive : { type: Number, required: false },
+  categories : { type: [ { type: String, required: true }], required: true },
+  price : { type: String, required: true },
+  regionID : { type: ObjectId, required: true },
+  objCreatedAt : { type: Date, default: Date.now }
 });
 
-//SETS OBJECT CREATED AT DATE TIME
-VendorSchema.pre('save', next => {
-  const now = new Date();
-
-  if(!this.createdAt) {
-    this.objCreatedAt = now;
+const MenuSchema = new mongoose.Schema({
+  section : { type: String, required: true },
+  items : {
+    type: [
+      {
+        name : { type: String, required: true },
+        price: { type: String, required: false }
+      }
+    ],
+    required: false
   }
-
-  return next();
 });
 
-module.exports = mongoose.model('book', BookSchema);
+const TweetSchema = new mongoose.Schema({
+  tweetID : { type: String, required: true },
+  createdAt : { type: Date, required: true },
+  text : { type: String, required: true },
+  userID : { type: String, required: true },
+  userName : { type: String, required: true },
+  userScreenName : { type: String, required: true },
+  geolocation : { type: CoordinatesSchema, required: false }
+})
+
+const CoordinatesSchema = new mongoose.Schema({
+  coordinatesDate: { type: Date, default: Date.now, required: true }
+  coordinates: {
+    type: [ { type: Number, required: true } ],
+    validate: [coordArrLimit, '{PATH} exceeds the limit of 2'],
+    required: true
+  }
+})
+
+const coordArrLimit = (val) => val.length <= 2;
+
+mongoose.model('Vendor', VendorSchema);
+mongoose.model('Menu', MenuSchema);
+mongoose.model('Tweet', TweetSchema);
+
+module.exports = mongoose;
