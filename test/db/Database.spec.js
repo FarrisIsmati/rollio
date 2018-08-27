a33f9cfc2f0f04d1cd02824b9417b64449f09c37
farris.ismati@gmail.com <farris.ismati@gmail.com>
2018-08-26 21:32:49 -0400

    Added more tests to Database.spec.js

--- test/db/Database.spec.js
+++ test/db/Database.spec.js
@@ -17,17 +17,68 @@ describe('Testing DB to the Seed File', function() {
   });
 
   describe('Region Collection', function() {
-      it('should have region name WASHINGTONDC', function(done) {
-        const washingtonName = "WASHINGTONDC";
-        Region.collection.findOne({ name: washingtonName})
-        .then(regionDC => {
-          expect(regionDC).to.exist;
-          expect(regionDC.name).to.equal(washingtonName);
+    describe('Washington DC', function() {
+      let region;
+
+      before(function(done) {
+        Region.collection.findOne({ name: 'WASHINGTONDC'})
+        .then(dc => {
+          region = dc;
           done();
         })
+      })
+
+      it('should exist', function(done) {
+        expect(region).to.exist;
+        done();
       });
+
+      it('should have region name WASHINGTONDC', function(done) {
+        expect(region.name).to.equal('WASHINGTONDC');
+        done();
+      });
+
+      it('should have timezone EST', function(done) {
+        expect(region.timezone).to.equal('EST');
+        done();
+      });
+    });
   });
-  
+
+  describe('Vendor Collection', function() {
+    let vendor;
+
+    describe('Yummies', function() {
+      before(function(done) {
+        Vendor.collection.findOne({ name: 'Yummies'})
+        .then(yummies => {
+          vendor = yummies;
+          done();
+        })
+      })
+
+      it('should exist', function(done) {
+        expect(vendor).to.exist;
+        done();
+      })
+
+      it('should have vendor name Yummies', function(done) {
+        expect(vendor.name).to.equal('Yummies');
+        done();
+      });
+
+      it('should have vendor menu', function(done) {
+        expect(vendor.menu).to.exist;
+        done();
+      });
+
+      it('should have vendor menu', function(done) {
+        expect(vendor.menu).to.exist;
+        done();
+      });
+    })
+  });
+
   after(function(done) {
     seed.emptyRegionsCollection()
     .then(() => seed.emptyVendors())
