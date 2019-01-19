a40756ea07487bd2f85f35feb5ff7086559978ff
Farris Ismati <farris.ismati@gmail.com>
2019-01-19 09:54:22 -0500

    Removed twitter service functionality and dependencies from Vendor Service

--- controllers/live/operations/DataOperations.js
+++ controllers/live/operations/DataOperations.js
@@ -1,6 +1,4 @@
 //DEPENDENCIES
-const Twitter             = require('../twitter/TwitterClient');
-const tweetParser         = require('../twitter/tweetParser');
 const redisOperations     = require('../../db/redis/redisOperations');
 
 //OPERATIONS
@@ -10,47 +8,6 @@ const vendorOperations     = require('../../db/operations/vendorOperations');
 class DataOperations {
   constructor(regionName) {
     this.regionName = regionName;
-    this.twitterClient = new Twitter({
-      c_key: process.env.TWITTER_CONSUMER_KEY,
-      c_secret: process.env.TWITTER_CONSUMER_SECRET,
-      a_key: process.env.TWITTER_ACCESS_TOKEN,
-      a_secret: process.env.TWITTER_ACCESS_SECRET,
-      regionName: this.regionName
-    });
-  }
-
-  async runOperations() {
-    // const self = this;
-    // this.twitterClient.streamClient(async e => {
-    //   const vendorTweet = await this.vendorTweetUpdate(e);
-    //   console.log(e);
-    //   const tweetAddress = tweetParser.scanAddress(vendorTweet);
-    //   vendorAddressUpdate(tweetAddress);
-    // });
-
-    //Strictly for testing sample twitter data (make sure you comment out the above code)
-    this.testRunOperations();
-  }
-
-  //Strictly for testing sample twitter data
-  async testRunOperations() {
-    const region = await regionOperations.getRegionByName(this.regionName);
-    const sampleData = require('../data/sampledata');
-
-    //Run through sample data in an async for loop
-    for (let i = 0, p = Promise.resolve(); i < sampleData.length; i++) {
-      p = p.then(_ => new Promise(async resolve => {
-          let tweet = sampleData[i];
-          //Update tweet first
-          let vendor = await vendorOperations.getVendorByTwitterID(region._id, tweet.twitterID);
-          await vendorOperations.updateVendorPush({ regionID: region._id, vendorID: vendor._id, field: 'tweetsDaily', payload: tweet});
-          //Check if tweet has an address
-          let tweetAddress = tweetParser.scanAddress(tweet);
-          await this.vendorAddressUpdate(tweetAddress);
-          resolve();
-        }
-      ));
-    }
   }
 
   async vendorAddressUpdate(payload) {
