6ade7e4ab3d261048f84f866c93fbc453e8bb1e7
farris.ismati@gmail.com <farris.ismati@gmail.com>
2019-04-21 18:29:31 -0400

    linted db seeds tests

--- test/db/seeds/async-seed.spec.js
+++ test/db/seeds/async-seed.spec.js
@@ -1,33 +1,37 @@
-//DEPENDENCIES
-const mongoose = require('../../../lib/db/mongo/mongoose/index');
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-undef */
+// DEPENDENCIES
 const chai = require('chai');
-const expect = chai.expect;
-//SCHEMAS
+const mongoose = require('../../../lib/db/mongo/mongoose/index');
+
+const { expect } = chai;
+
+// SCHEMAS
 const Region = mongoose.model('Region');
-const Vendor = mongoose.model('Vendor');
-//SEED
+
+// SEED
 const seed = require('../../../lib/db/mongo/seeds/dev-seed');
 
-describe('Asynchronous Vendor Update Operations', function() {
-  describe('Yelp', function() {
+describe('Asynchronous Vendor Update Operations', () => {
+  describe('Yelp', () => {
     let regionID;
 
     before(async () => {
       await seed.seedRegions();
-      regionID = await Region.findOne({name: 'WASHINGTONDC'}).then(res => res._id);
+      regionID = await Region.findOne({ name: 'WASHINGTONDC' }).then(res => res._id);
     });
 
-    it('Expect asyncUpdateVendor to return yelp rating, and yelp price in payload', async function() {
-      //PEPE FOODTRUCK ID
-      const vendor = {name: "PEPE", yelpId: "xF5cphbxvMKNdMRNSWAzkQ"};
-      const payload = await seed.asyncUpdateVendor({vendor, regionID, isTestEnv: false});
+    it('Expect asyncUpdateVendor to return yelp rating, and yelp price in payload', async () => {
+      // PEPE FOODTRUCK ID
+      const vendor = { name: 'PEPE', yelpId: 'xF5cphbxvMKNdMRNSWAzkQ' };
+      const payload = await seed.asyncUpdateVendor({ vendor, regionID, isTestEnv: false });
       expect(payload.yelpRating).to.exist;
       expect(payload.price).to.exist;
       expect(payload.yelpRating).to.equal(4);
       expect(payload.price).to.equal('$$');
     });
 
-    after(done => { seed.emptyRegions().then(() => seed.emptyVendors()).then(() => done()) });
+    after((done) => { seed.emptyRegions().then(() => seed.emptyVendors()).then(() => done()); });
   });
-
 });
--- test/db/seeds/dev-seed.spec.js
+++ test/db/seeds/dev-seed.spec.js
@@ -1,21 +1,25 @@
-//DEPENDENCIES
-const mongoose = require('../../../lib/db/mongo/mongoose/index');
+/* eslint-disable no-undef */
+/* eslint-disable no-underscore-dangle */
+// DEPENDENCIES
 const chai = require('chai');
-const expect = chai.expect;
-//SCHEMAS
+const mongoose = require('../../../lib/db/mongo/mongoose/index');
+
+const { expect } = chai;
+// SCHEMAS
 const Region = mongoose.model('Region');
 const Vendor = mongoose.model('Vendor');
-//SEED
+// SEED
 const seed = require('../../../lib/db/mongo/seeds/dev-seed');
 
-describe('Development Seed Operations', function() {
-  const seedDB = done => { seed.runSeed().then(() => done()) };
-  const emptyDB = done => { seed.emptyRegions().then(() => seed.emptyVendors()).then(() => done()) };
+describe('Development Seed Operations', () => {
+  const seedDB = (done) => { seed.runSeed().then(() => done()); };
+  // eslint-disable-next-line max-len
+  const emptyDB = (done) => { seed.emptyRegions().then(() => seed.emptyVendors()).then(() => done()); };
 
-  describe('Empty Regions', function() {
+  describe('Empty Regions', () => {
     before(done => seedDB(done));
 
-    it('Expect region collection to be empty', async function() {
+    it('Expect region collection to be empty', async () => {
       await seed.emptyRegions();
       const regionsAfter = await Region.find({});
       expect(regionsAfter.length).to.equal(0);
@@ -24,10 +28,10 @@ describe('Development Seed Operations', function() {
     after(done => emptyDB(done));
   });
 
-  describe('Empty Vendors', function() {
+  describe('Empty Vendors', () => {
     before(done => seedDB(done));
 
-    it('Expect vendor collection to be empty', async function() {
+    it('Expect vendor collection to be empty', async () => {
       await seed.emptyVendors();
       const vendorsAfter = await Vendor.find({});
       expect(vendorsAfter.length).to.equal(0);
@@ -36,8 +40,8 @@ describe('Development Seed Operations', function() {
     after(done => emptyDB(done));
   });
 
-  describe('Seed Regions', function() {
-    it('Expect region collection length to be equal to 1', async function() {
+  describe('Seed Regions', () => {
+    it('Expect region collection length to be equal to 1', async () => {
       await seed.seedRegions();
       const regionsAfter = await Region.find({});
       expect(regionsAfter.length).to.equal(1);
@@ -46,21 +50,20 @@ describe('Development Seed Operations', function() {
     after(done => emptyDB(done));
   });
 
-  describe('Seed Vendors', function() {
+  describe('Seed Vendors', () => {
     let regionID;
 
     before(async () => {
       await seed.seedRegions();
-      regionID = await Region.findOne({name: 'WASHINGTONDC'}).then(res => res._id);
+      regionID = await Region.findOne({ name: 'WASHINGTONDC' }).then(res => res._id);
     });
 
-    it('Expect vendors in WASHINGTONDC to be length to be equal to 5', async function() {
+    it('Expect vendors in WASHINGTONDC to be length to be equal to 5', async () => {
       await seed.seedVendors('WASHINGTONDC');
-      const vendorsAfter = await Vendor.find({regionID});
+      const vendorsAfter = await Vendor.find({ regionID });
       expect(vendorsAfter.length).to.equal(5);
     });
 
     after(done => emptyDB(done));
   });
-
 });
