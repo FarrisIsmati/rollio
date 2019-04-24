76f5d9cfab1c8e9fda6c9f32441789fa42f9890a
farris.ismati@gmail.com <farris.ismati@gmail.com>
2019-04-23 22:00:54 -0400

    linted message index file and message index test file

--- test/messaging/index.spec.js
+++ test/messaging/index.spec.js
@@ -1,33 +1,37 @@
-//DEPENDENCIES
-const rabbitmq = require('../../lib/messaging/index');
+// DEPENDENCIES
 const chai = require('chai');
-const expect = chai.expect;
+const rabbitmq = require('../../lib/messaging/index');
 
-describe('RabbitMQ', function() {
-  describe('Recieve from "test" channel', function() {
-    it('Expect RabbitMQ instance to be connected', async function() {
+const { expect } = chai;
+
+describe('RabbitMQ', () => {
+  describe('Recieve from "test" channel', () => {
+    it('Expect RabbitMQ instance to be connected', async () => {
       const amqp = await rabbitmq.amqp;
+
       expect(amqp).to.be.an('object');
     });
-    it('Expect "test" channel to send a message to the Queue', async function() {
-      const chName = 'testSend'
+
+    it('Expect "test" channel to send a message to the Queue', async () => {
+      const chName = 'testSend';
       const message = 'hello test send';
       const channel = await rabbitmq.amqp;
       await rabbitmq.send(chName, message);
-      await channel.assertQueue(chName, {durable: false});
-      await channel.consume(chName, msg => {
+      await channel.assertQueue(chName, { durable: false });
+      await channel.consume(chName, (msg) => {
         expect(JSON.parse(msg.content)).to.be.equal(message);
-      }, {noAck: true});
-    })
-    it('Expect "test" channel to recieve a message from the Queue', async function() {
-      const chName = 'testRecieve'
+      }, { noAck: true });
+    });
+
+    it('Expect "test" channel to recieve a message from the Queue', async () => {
+      const chName = 'testRecieve';
       const message = 'hello test recieve';
       const channel = await rabbitmq.amqp;
-      await channel.assertQueue(chName, {durable: false});
-      await channel.sendToQueue(chName, new Buffer(JSON.stringify(message)));
-      await rabbitmq.recieve(chName, msg => {
+      await channel.assertQueue(chName, { durable: false });
+      await channel.sendToQueue(chName, Buffer.from(JSON.stringify(message)));
+      await rabbitmq.recieve(chName, (msg) => {
         expect(JSON.parse(msg.content)).to.be.equal(message);
       });
-    })
+    });
   });
 });
