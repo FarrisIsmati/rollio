4ce1618e30d0bb7f5b6af376a29b350d98a9c693
farris.ismati@gmail.com <farris.ismati@gmail.com>
2019-04-23 21:40:41 -0400

    linted cache-middleware spec file

--- test/routes/cache-middleware.spec.js
+++ test/routes/cache-middleware.spec.js
@@ -1,50 +1,55 @@
-//DEPENDENCIES
-const mongoose = require('../../lib/db/mongo/mongoose/index');
+// DEPENDENCIES
 const chai = require('chai');
-const expect = chai.expect;
+
+const { expect } = chai;
 const sinonChai = require('sinon-chai');
 const sinonExpressMock = require('sinon-express-mock');
+const mongoose = require('../../lib/db/mongo/mongoose/index');
+
 const { mockReq, mockRes } = sinonExpressMock;
 const client = require('../../lib/db/redis/index');
-//MIDDLEWARE
+
+// MIDDLEWARE
 const { checkCache, regionRouteOps, vendorRouteOps } = require('../../lib/routes/middleware/db-operations');
-//SCHEMAS
+
+// SCHEMAS
 const Vendor = mongoose.model('Vendor');
 const Region = mongoose.model('Region');
-//SEED
+
+// SEED
 const seed = require('../../lib/db/mongo/seeds/dev-seed');
 
 chai.use(sinonChai);
 
-describe('Cache Middleware', function() {
+describe('Cache Middleware', () => {
   const regionName = 'WASHINGTONDC';
   const twitterId = '1053649707493404678';
   let regionId;
   let vendorId;
 
-  before(async function() {
+  before(async () => {
     await seed.runSeed().then(async () => {
-      regionId = await Region.findOne({'name': regionName}).then(region => region._id);
+      regionId = await Region.findOne({ name: regionName }).then(region => region._id);
       vendorId = await Vendor.findOne({
-        'regionID': regionId,
-        'twitterID': twitterId
+        regionID: regionId,
+        twitterID: twitterId,
       })
-      .then(vendor => vendor._id);
+        .then(vendor => vendor._id);
     });
   });
 
-  afterEach(async function() {
+  afterEach(async () => {
     await client.flushallAsync();
   });
 
-  describe('Check Cache Method', function() {
+  describe('Check Cache Method', () => {
     const body = {
       method: 'testGETCache',
       path: 'testPATHCache',
       params: {
-        id: 'region123'
-      }
-    }
+        id: 'region123',
+      },
+    };
 
     const req = mockReq(body);
     const res = mockRes();
@@ -52,55 +57,57 @@ describe('Cache Middleware', function() {
     const payload = {
       collectionKey: 'region',
       queryKey: `q::method::${req.method}::regionID::${req.path}`,
-      ops: () => 'test123'
-    }
+      ops: () => 'test123',
+    };
 
-    beforeEach(async function() {
+    beforeEach(async () => {
       await client.delAsync(payload.collectionKey, `q::method::${req.method}::regionID::${req.path}`);
-    })
+    });
 
-    it('expect to set data to a route that is not found in the cache', async function() {
+    it('expect to set data to a route that is not found in the cache', async () => {
       const checkCacheRes = await checkCache(req, res, payload);
       expect(checkCacheRes).to.equal(payload.ops());
     });
 
-    it('expect to set data to a route that is found in the cache', async function() {
+    it('expect to set data to a route that is found in the cache', async () => {
       const data = '123test';
-      const updatedPayload = { ...payload, ops: (req, res, cb) => {
-        return cb(data)
-      }}
+      const updatedPayload = {
+        ...payload,
+        // eslint-disable-next-line no-shadow
+        ops: (req, res, cb) => cb(data),
+      };
 
-      //Cache the data
+      // Cache the data
       await checkCache(req, res, updatedPayload);
-      //See if data was cached
-      const value = await client.hgetAsync(payload.collectionKey, payload.queryKey)
+      // See if data was cached
+      const value = await client.hgetAsync(payload.collectionKey, payload.queryKey);
 
       expect(value).to.equal(JSON.stringify(data));
     });
   });
 
-  describe('Get Region Route Ops Method', function() {
+  describe('Get Region Route Ops Method', () => {
     let body;
     let req;
     let res;
 
-    before(function(){
-       body = {
+    before(() => {
+      body = {
         method: 'GET',
         path: regionId.toString(),
         params: {
-          id: regionId
-        }
-      }
-       req = mockReq(body);
-       res = mockRes();
+          id: regionId,
+        },
+      };
+      req = mockReq(body);
+      res = mockRes();
     });
 
-    it('expect Region Route Operations getRegionId method to save the req path into the cache', async function() {
+    it('expect Region Route Operations getRegionId method to save the req path into the cache', async () => {
       const isInCacheBefore = await client.hgetAsync('region', `q::method::GET::path::${regionId}`);
       await regionRouteOps.getRegionId(req, res);
       const isInCacheAfter = await client.hgetAsync('region', `q::method::GET::path::${regionId}`)
-      .then(cachedRes => JSON.parse(cachedRes));
+        .then(cachedRes => JSON.parse(cachedRes));
 
       expect(isInCacheBefore).to.be.null;
       expect(isInCacheAfter).to.be.an('object');
@@ -108,49 +115,49 @@ describe('Cache Middleware', function() {
     });
   });
 
-  describe('Get Vendor Route Ops Method', function() {
+  describe('Get Vendor Route Ops Method', () => {
     let body;
     let res;
 
-    before(function(){
+    before(() => {
       body = {
         method: 'GET',
         path: `/${regionId.toString()}`,
         params: {
-          regionID: regionId
-        }
-      }
+          regionID: regionId,
+        },
+      };
       res = mockRes();
     });
 
-    it('expect Vendor Route Operations getVendorById method to save the req path into the cache', async function() {
-      bodyVendorById = {
+    it('expect Vendor Route Operations getVendorById method to save the req path into the cache', async () => {
+      const bodyVendorById = {
         ...body,
         path: `/${regionId.toString()}/${vendorId.toString()}`,
         params: {
           regionID: regionId,
-          vendorID: vendorId
-        }
-      }
+          vendorID: vendorId,
+        },
+      };
       const reqVendorById = mockReq(bodyVendorById);
 
       const isInCacheBefore = await client.hgetAsync('vendor', `q::method::GET::path::/${regionId}/${vendorId}`);
       await vendorRouteOps.getVendorById(reqVendorById, res);
       const isInCacheAfter = await client.hgetAsync('vendor', `q::method::GET::path::/${regionId}/${vendorId}`)
-      .then(cachedRes => JSON.parse(cachedRes));
+        .then(cachedRes => JSON.parse(cachedRes));
 
       expect(isInCacheBefore).to.be.null;
       expect(isInCacheAfter).to.be.an('object');
       expect(isInCacheAfter).to.have.own.property('twitterID');
     });
 
-    it('expect Vendor Route Operations getVendors method to save the req NON QS path into the cache', async function() {
+    it('expect Vendor Route Operations getVendors method to save the req NON QS path into the cache', async () => {
       const reqGetVendors = mockReq(body);
 
       const isInCacheBefore = await client.hgetAsync('vendor', `q::method::GET::path::/${regionId}`);
       await vendorRouteOps.getVendors(reqGetVendors, res);
       const isInCacheAfter = await client.hgetAsync('vendor', `q::method::GET::path::/${regionId}`)
-      .then(cachedRes => JSON.parse(cachedRes));
+        .then(cachedRes => JSON.parse(cachedRes));
 
       expect(isInCacheBefore).to.be.null;
       expect(isInCacheAfter).to.be.an('array');
@@ -158,16 +165,17 @@ describe('Cache Middleware', function() {
     });
   });
 
-  describe('Put Vendor Route Ops Method', function() {
+  describe('Put Vendor Route Ops Method', () => {
     let res;
     let locationId;
 
-    before(async function() {
-      locationId = await Vendor.findOne({'_id': vendorId}).then(vendor => vendor.locationHistory[0]._id);
+    before(async () => {
+      locationId = await Vendor.findOne({ _id: vendorId })
+        .then(vendor => vendor.locationHistory[0]._id);
       res = mockRes();
     });
 
-    it('expect putRegionIdVendorIdLocationTypeLocationIDAccuracy method to clear the cache on path regionId/vendorId', async function() {
+    it('expect putRegionIdVendorIdLocationTypeLocationIDAccuracy method to clear the cache on path regionId/vendorId', async () => {
       const bodyPutVendorLocationAccuracy = {
         method: 'PUT',
         params: {
@@ -177,37 +185,38 @@ describe('Cache Middleware', function() {
         body: {
           type: 'locationHistory',
           locationID: locationId,
-          amount: 1
-        }
-      }
+          amount: 1,
+        },
+      };
       const bodyVendorById = {
         method: 'GET',
         path: `/${regionId.toString()}/${vendorId.toString()}`,
         params: {
           regionID: regionId,
-          vendorID: vendorId
-        }
-      }
+          vendorID: vendorId,
+        },
+      };
       const reqPutVendorLocationAccuracy = mockReq(bodyPutVendorLocationAccuracy);
       const reqVendorById = mockReq(bodyVendorById);
 
       await vendorRouteOps.getVendorById(reqVendorById, res);
       const isInCacheBefore = await client.hgetAsync('vendor', `q::method::GET::path::/${regionId}/${vendorId}`)
-      .then(cachedRes => JSON.parse(cachedRes));
+        .then(cachedRes => JSON.parse(cachedRes));
 
-      await vendorRouteOps.putRegionIdVendorIdLocationTypeLocationIDAccuracy(reqPutVendorLocationAccuracy, res);
+      await vendorRouteOps
+        .putRegionIdVendorIdLocationTypeLocationIDAccuracy(reqPutVendorLocationAccuracy, res);
 
       const isInCacheAfter = await client.hgetAsync('vendor', `q::method::GET::path::/${regionId}/${vendorId}`)
-      .then(cachedRes => JSON.parse(cachedRes));
+        .then(cachedRes => JSON.parse(cachedRes));
 
       expect(isInCacheBefore).to.be.an('object');
       expect(isInCacheAfter).to.be.null;
     });
   });
 
-  after(function(done) {
+  after((done) => {
     seed.emptyRegions()
-    .then(() => seed.emptyVendors())
-    .then(() => done());
+      .then(() => seed.emptyVendors())
+      .then(() => done());
   });
-})
+});
