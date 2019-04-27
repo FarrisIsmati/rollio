000b211c3e17e4ced1ffdab402710b12d3f0c66f
farris.ismati@gmail.com <farris.ismati@gmail.com>
2019-04-27 09:20:17 -0400

    linted tweet-parser.spec and location-ops.spec

--- test/twitter/tweet-parser.spec.js
+++ test/twitter/tweet-parser.spec.js
@@ -1,32 +1,33 @@
-//DEPENDENCIES
+// DEPENDENCIES
 const chai = require('chai');
-const expect = chai.expect;
+
+const { expect } = chai;
 const tweetParser = require('../../lib/twitter/parse/tweet-parser');
 
-//DATA
+// DATA
 const tweets = require('./data.json');
 
-describe('Tweet Parser', function() {
-  describe('Scan Address', function() {
-    it('Expect tweet with text "Erlich Bachman" to not return a match', function() {
+describe('Tweet Parser', () => {
+  describe('Scan Address', () => {
+    it('Expect tweet with text "Erlich Bachman" to not return a match', () => {
       const result = tweetParser.scanAddress(tweets.formatted1);
       expect(result.match).to.be.false;
     });
-    it('Expect tweet with text "Meet us at Union Station" to return a match', function() {
+    it('Expect tweet with text "Meet us at Union Station" to return a match', () => {
       const result = tweetParser.scanAddress(tweets.formatted2);
       expect(result.location.address).to.equal('50 Massachusetts Ave NE, Washington, DC 20002');
       expect(result.match).to.be.true;
     });
   });
-  describe('Match Known Location', function() {
-    it('Expect tweet with text "Find us at roslyn today." to return a match', function() {
+  describe('Match Known Location', () => {
+    it('Expect tweet with text "Find us at roslyn today." to return a match', () => {
       const result = tweetParser.matchKnownLocation(tweets.formatted4);
       expect(result.location.address).to.equal('Rosslyn, Virginia  22209');
       expect(result.match).to.be.true;
     });
   });
-  describe('Match Phrase', function() {
-    it('Expect tweet with text "Find us at roslyn tomorrow." not to return a match', function() {
+  describe('Match Phrase', () => {
+    it('Expect tweet with text "Find us at roslyn tomorrow." not to return a match', () => {
       const result = tweetParser.matchPhrase(tweets.formatted3);
       expect(result.match).to.be.false;
     });
