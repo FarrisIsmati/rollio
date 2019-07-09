9bb8fa4238b5ecc9cd1ba5097f780664f5ca3b51
farris.ismati@gmail.com <farris.ismati@gmail.com>
2019-07-08 22:32:14 -0400

    added eslint airbnb for react

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } 
+from 'react';
 
 class App extends Component {
   render() {
-    return (
+return (
       <div className="App">
         <div className="button__two">
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
+          <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
   }
 }
-
+var lol = potato
 export default App;
