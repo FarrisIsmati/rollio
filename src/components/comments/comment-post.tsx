00e94fb48340db82e42263da7114170af3b28444
farris.ismati@gmail.com <farris.ismati@gmail.com>
2019-08-02 21:16:49 -0400

    Added props to posted comment

--- src/components/comments/comment-post.tsx
+++ src/components/comments/comment-post.tsx
@@ -1,15 +1,21 @@
 // DEPENDENCIES
 import React, {FC} from 'react';
 
-const CommentPost:FC = () => {
+interface CommentPostProps  {
+  time: string,
+  name: string,
+  text: string
+}
+
+const CommentPost = (props: CommentPostProps) => {
   return (
     <div className="commentpost__wrapper"> 
         <div className="commentpost__header_wrapper">
-            <p className="font__comment_posted_name">Johnny</p>
-            <p className="font__comment_posted_time">1min</p>
+            <p className="font__comment_posted_name">{ props.name }</p>
+            <p className="font__comment_posted_time">{ props.time }</p>
         </div>
         <div className="commentpost__content_wrapper">
-            <p className="font__comment_posted_content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p className="font__comment_posted_content">{ props.text }</p>
         </div>
     </div>
   );
--- src/components/truck-profile/truck-profile.tsx
+++ src/components/truck-profile/truck-profile.tsx
@@ -9,8 +9,8 @@ const TruckProfile = () => {
   return (
       // Mobile resize this flex centers
     <div className="truckprofile__wrapper"> 
-        <CommentAdd/>
-        {/* <CommentPost/> */}
+        {/* <CommentAdd/> */}
+        <CommentPost name={'Jim'} time={'1min'} text={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}/>
     </div>
   );
 }
