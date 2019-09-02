0acf323f10b39748a758fe65e5246ba070e22c28
farris.ismati@gmail.com <farris.ismati@gmail.com>
2019-09-02 12:28:52 -0400

    Got a default google maps inside

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,10 +1,23 @@
 // DEPENDENCIES
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+
+// CONFIG
+import { GOOGLE_MAP_API_KEY } from '../../config'
 
 const Map = () => {
   return (
     <div className='map__wrapper'>
-        map
+       <GoogleMapReact
+          // @ts-ignore
+          bootstrapURLKeys={{ key: GOOGLE_MAP_API_KEY }}
+          defaultCenter={{
+            lat: 59.95,
+            lng: 30.33
+          }}
+          defaultZoom={11}
+        >
+      </ GoogleMapReact>
     </div>
   );
 }
