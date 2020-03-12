ba1c7f0c5249847e7f59d6a8b520cac8377b517d
sloan-holzman <sloan.holzman@gmail.com>
2020-03-11 21:14:39 -0400

    add nlp service (WIP)

---
+++ rollio-service-nlp/python/update_model.py
@@ -0,0 +1,39 @@
+import spacy
+import random
+
+PATH = './tweet_model'
+nlp = spacy.load(PATH)
+TRAIN_DATA = []
+
+#
+# def train_spacy(data,iterations):
+#     TRAIN_DATA = data
+#     nlp = spacy.load(PATH)
+#
+#     # get names of other pipes to disable them during training
+#     other_pipes = [pipe for pipe in nlp.pipe_names if pipe != 'ner']
+#     with nlp.disable_pipes(*other_pipes):  # only train NER
+#         optimizer = nlp.begin_training()
+#         for itn in range(iterations):
+#             print("Starting iteration " + str(itn))
+#             random.shuffle(TRAIN_DATA)
+#             losses = {}
+#             for text, annotations in TRAIN_DATA:
+#                 nlp.update(
+#                     [text],  # batch of texts
+#                     [annotations],  # batch of annotations
+#                     drop=0.2,  # dropout - make it harder to memorise data
+#                     sgd=optimizer,  # callable to update weights
+#                     losses=losses)
+#             print(losses)
+#     return nlp
+
+
+# train_spacy(TRAIN_DATA, 20)
+nlp.to_disk(PATH)
+
+#Test your text
+test_text = input("Enter your testing text: ")
+doc = nlp(test_text)
+for ent in doc.ents:
+    print(ent.text, ent.start_char, ent.end_char, ent.label_)
