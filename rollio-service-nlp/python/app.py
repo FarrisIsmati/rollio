b986c238292dbd84a7c619ff584c6d33ae644fef
sloan-holzman <sloan.holzman@gmail.com>
2020-03-16 20:53:04 -0400

    update app to return entities; create parse.py

--- rollio-service-nlp/python/app.py
+++ rollio-service-nlp/python/app.py
@@ -1,17 +1,18 @@
 from flask import Flask, request, jsonify, Response
 import json
+from parse import parse_tweet
 app = Flask(__name__)
 
 @app.route("/parse-location", methods=['POST'])
 def extract_schema():
     try:
         payload = request.get_json()
-        print("!!!", payload, "!!!")
-        response = None
+        response = []
 
         if "text" in payload:
-            print(payload.get("text"))
-            response = "blah"
+            parsed_entities = parse_tweet(payload.get("text"))
+            for ent in parsed_entities:
+                response.append({'text': ent.text, 'start': ent.start_char, 'end': ent.end_char, 'label': ent.label_});
 
         else:
             raise Exception( "Missing required field text")
