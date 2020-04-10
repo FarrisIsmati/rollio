c893ed2f91d5df0b820efa2940340b754e2b4c7e
farris.ismati@gmail.com <farris.ismati@gmail.com>
2020-04-10 17:01:06 -0400

    setup winston for aws

--- rollio-service-vendor/lib/log/index.js
+++ rollio-service-vendor/lib/log/index.js
@@ -1,4 +1,7 @@
+// DEPENDENCIES
 const winston = require('winston');
+const WinstonCloudWatch = require('winston-cloudwatch');
+const moment = require('moment');
 const config = require('../../config');
 
 const { format } = winston;
@@ -7,7 +10,12 @@ const { label } = format;
 const getLabel = callingModule => `[${callingModule}]`;
 
 module.exports = (callingModule) => {
-  const logger = winston.createLogger({
+  const now = moment();
+
+  const isAWS = config.NODE_ENV !== 'DEVELOPMENT_LOCAL' || config.NODE_ENV !== 'TEST_LOCAL' || config.NODE_ENV !== 'TEST_DOCKER';
+
+  // eslint-disable-next-line new-cap
+  const logger = new winston.createLogger({
     levels: winston.config.syslog.levels,
     format: format.combine(
       label({ label: getLabel(callingModule) }),
@@ -16,17 +24,30 @@ module.exports = (callingModule) => {
       }),
       format.json(),
     ),
-    transports: [
+    // If we log to AWS use don't transport to a local file
+    transports: isAWS ? [
+      new (winston.transports.Console)({
+        timestamp: true,
+        colorize: true,
+      }),
+    ] : [
       new winston.transports.File({ name: 'console.error', filename: 'error.log', level: 'error' }),
       new winston.transports.File({ name: 'console.info', filename: 'combined.log' }),
     ],
   });
 
-  //
-  // If we're not in production then log to the `console` with the format:
-  // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-  //
-  if (config.NODE_ENV === 'DEVELOPMENT_LOCAL' || config.NODE_ENV === 'DEVELOPMENT_DOCKER') {
+  const cloudwatchConfig = {
+    logGroupName: 'rollio-service-vendor-dev',
+    logStreamName: `rollio-service-vendor -  ${now.format('YYYY-MM-DD')}`,
+    awsAccessKeyId: config.CLOUDWATCH_ACCESS_KEY_ID,
+    awsSecretKey: config.CLOUDWATCH_SECRET_ACCESS_KEY,
+    awsRegion: config.CLOUDWATCH_REGION,
+    messageFormatter: ({ level, message, additionalInfo }) => `[${level}] : ${message} \nAdditional Info: ${JSON.stringify(additionalInfo)}}`,
+  };
+
+  if (isAWS) {
+    logger.add(new WinstonCloudWatch(cloudwatchConfig));
+  } else if (config.NODE_ENV === 'DEVELOPMENT_DOCKER') {
     logger.add(new winston.transports.Console({
       format: winston.format.simple(),
     }));
