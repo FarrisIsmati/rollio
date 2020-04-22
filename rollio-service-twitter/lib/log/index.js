bbce4cd662a4b195e8bac6bdacce4e460c2315c5
farris.ismati@gmail.com <farris.ismati@gmail.com>
2020-04-22 16:02:57 -0400

    Updated logger in twitter service

--- rollio-service-twitter/lib/log/index.js
+++ rollio-service-twitter/lib/log/index.js
@@ -2,29 +2,34 @@ const winston = require('winston');
 const config = require('../../config');
 
 const { format } = winston;
+const { label } = format;
 
-const logger = winston.createLogger({
-  levels: winston.config.syslog.levels,
-  format: format.combine(
-    format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss',
-    }),
-    format.json(),
-  ),
-  transports: [
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
-  ],
-});
+const getLabel = callingModule => `[${callingModule}]`;
 
-//
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
-if (config.NODE_ENV !== 'PRODUCTION') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple(),
-  }));
-}
+module.exports = (callingModule) => {
+  const isAWS = config.NODE_ENV === 'DEVELOPMENT_DOCKER' || config.NODE_ENV === 'PRODUCTION' || config.NODE_ENV === 'TEST_DOCKER';
 
-module.exports = logger;
+  const logger = winston.createLogger({
+    levels: winston.config.syslog.levels,
+    format: format.combine(
+      label({ label: getLabel(callingModule) }),
+      format.timestamp({
+        format: 'YYYY-MM-DD HH:mm:ss',
+      }),
+      format.json(),
+    ),
+    transports: isAWS ? [
+      new winston.transports.Console({
+        format: winston.format.simple(),
+      }),
+    ] : [
+      new winston.transports.File({ name: 'console.error', filename: 'error.log', level: 'error' }),
+      new winston.transports.File({ name: 'console.info', filename: 'combined.log' }),
+      new winston.transports.Console({
+        format: winston.format.simple(),
+      }),
+    ],
+  });
+
+  return logger;
+};
