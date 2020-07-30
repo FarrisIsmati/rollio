fbee30c9a24a44432ad2afe429e50442c3c0db97
farris.ismati@gmail.com <farris.ismati@gmail.com>
2020-07-29 21:48:26 -0400

    worked on location schema unit tests

--- rollio-service-vendor/test/unit/db/Schemas.spec.js
+++ rollio-service-vendor/test/unit/db/Schemas.spec.js
@@ -1,10 +1,8 @@
 // DEPENDENCIES
 const chai = require('chai');
-const moment = require('moment');
+const asserttype = require('chai-asserttype');
 const { ObjectId } = require('mongoose').Types;
 const mongoose = require('../../../lib/db/mongo/mongoose/index');
-
-const { expect } = chai;
 
 // SCHEMAS
 const Comment = mongoose.model('Comment');
@@ -13,8 +11,12 @@ const Location = mongoose.model('Location');
 const Vendor = mongoose.model('Vendor');
 const User = mongoose.model('User');
 
+const { expect } = chai;
+chai.use(asserttype);
+
 // TESTS
 describe('Schemas', () => {
+  // Location data object used across multiple schemas
   const initLocationData = {
     vendorID: new ObjectId(),
     locationDate: Date.now(),
@@ -103,58 +105,198 @@ describe('Schemas', () => {
       const region = new Region(regionData);
       region.validate((err) => {
         expect(Object.keys(err.errors).length).to.be.equal(1);
-        expect(err.errors.timezone).to.exist;
         expect(err.errors).to.include.keys('timezone');
         done();
       });
     });
   });
 
-//   describe('Location Schema', () => {
-//     it('expect to be invalid if there are more than three values in the coordinates field', (done) => {
-//       const coordinates = new Location({
-//         coordinates: { lat: 3.42424 },
-//       });
-//       coordinates.validate((err) => {
-//         expect(err.errors['coordinates.long']).to.exist;
-//         expect(err.errors['coordinates.long'].message).to.equal('Path `coordinates.long` is required.');
-//         done();
-//       });
-//     });
+  describe('Location Schema', () => {
+    it('expect to be invalid if vendorID is not provided', (done) => {
+      const locationData = {...initLocationData};
+      delete locationData.vendorID;
 
-//     it('expect to be invalid if the coordinates are out of the lat long ranges', (done) => {
-//       const coordinates = new Location({
-//         coordinates: { lat: 5000, long: 5000 },
-//       });
-//       coordinates.validate((err) => {
-//         expect(err.errors['coordinates.long']).to.exist;
-//         expect(err.errors['coordinates.long'].message).to.equal('Longitude is not in the correct range');
-//         expect(err.errors['coordinates.lat']).to.exist;
-//         expect(err.errors['coordinates.lat'].message).to.equal('Latitude is not in the correct range');
-//         done();
-//       });
-//     });
+      const location = new Location(locationData);
+      location.validate((err) => {
+        expect(Object.keys(err.errors).length).to.be.equal(1);
+        expect(err.errors).to.include.keys('vendorID');
+        done();
+      });
+    });
 
-//     it('expect default startDate, endDate, and locationDate to be set correctly', (done) => {
-//       const newLocation = new Location({ vendorID: mongoose.Types.ObjectId(), address: '123 street', coordinates: { lat: 5, long: 5 } });
+    it('expect location date to exist after creation when not provided', (done) => {
+      const locationData = {...initLocationData};
+      delete locationData.locationDate;
 
-//       newLocation.save((err, location) => {
-//         if (err) {
-//           // Error won't show without this log
-//           // eslint-disable-next-line no-console
-//           console.log(err);
-//         }
+      const location = new Location(locationData);
+      location.validate((err) => {
+        expect(err).to.be.null;
+        expect(location.locationDate).to.be.date();
+        done();
+      });
+    });
 
-//         const { locationDate, startDate, endDate } = location;
-//         const endOfTomorrow = moment(new Date()).endOf('day').toDate();
-//         const currentDate = moment().format();
-//         expect(endDate).to.be.eql(endOfTomorrow);
-//         expect(moment(locationDate).format()).to.be.eql(currentDate);
-//         expect(moment(startDate).format()).to.be.eql(currentDate);
-//         done();
-//       });
-//     });
-//   });
+    it('expect accuracy to exist after creation when not provided', (done) => {
+      const locationData = {...initLocationData};
+      delete locationData.accuracy;
+
+      const location = new Location(locationData);
+      location.validate((err) => {
+        expect(err).to.be.null;
+        expect(location.accuracy).to.be.equal(0);
+        done();
+      });
+    });
+
+    it('expect to be invalid if address is not provided', (done) => {
+      const locationData = {...initLocationData};
+      delete locationData.address;
+
+      const location = new Location(locationData);
+      location.validate((err) => {
+        expect(Object.keys(err.errors).length).to.be.equal(1);
+        expect(err.errors).to.include.keys('address');
+        done();
+      });
+    });
+
+    it('expect to be valid if city is not provided', (done) => {
+      const locationData = {...initLocationData};
+      delete locationData.city;
+
+      const location = new Location(locationData);
+      location.validate((err) => {
+        expect(err).to.be.null;
+        done();
+      });
+    });
+
+    it('expect to be valid if neighborhood is not provided', (done) => {
+      const locationData = {...initLocationData};
+      delete locationData.neighborhood;
+
+      const location = new Location(locationData);
+      location.validate((err) => {
+        expect(err).to.be.null;
+        done();
+      });
+    });
+
+    it('expect to be valid if tweetID is not provided', (done) => {
+      const locationData = {...initLocationData};
+      delete locationData.tweetID;
+
+      const location = new Location(locationData);
+      location.validate((err) => {
+        expect(err).to.be.null;
+        done();
+      });
+    });
+
+    it('expect start date to exist after creation when not provided', (done) => {
+      const locationData = {...initLocationData};
+      delete locationData.startDate;
+
+      const location = new Location(locationData);
+      location.validate((err) => {
+        expect(err).to.be.null;
+        expect(location.startDate).to.be.date();
+        done();
+      });
+    });
+
+    it('expect end date to exist after creation when not provided', (done) => {
+      const locationData = {...initLocationData};
+      delete locationData.endDate;
+
+      const location = new Location(locationData);
+      location.validate((err) => {
+        expect(err).to.be.null;
+        expect(location.endDate).to.be.date();
+        done();
+      });
+    });
+
+    it('expect to be invalid if match method is not within schema matchMethod enum', (done) => {
+      const locationData = {...initLocationData};
+
+      const invalidValue = 'Test 123';
+      locationData.matchMethod = invalidValue;
+
+      const location = new Location(locationData);
+      location.validate((err) => {
+        expect(Object.keys(err.errors).length).to.be.equal(1);
+        expect(err.errors).to.include.keys('matchMethod');
+        done();
+      });
+    });
+
+    it('expect to be valid if match method is within schema matchMethod enum', (done) => {
+      const locationData = {...initLocationData};
+
+      const validValue = 'Vendor Input';
+      locationData.matchMethod = validValue;
+
+      const location = new Location(locationData);
+      location.validate((err) => {
+        expect(err).to.be.null;
+        expect(location.matchMethod).to.be.equal(validValue);
+        done();
+      });
+    });
+
+    it('expect overridden to exist and be false after creation when not provided', (done) => {
+      const locationData = {...initLocationData};
+      delete locationData.overridden;
+
+      const location = new Location(locationData);
+      location.validate((err) => {
+        expect(err).to.be.null;
+        expect(location.overridden).to.be.false;
+        done();
+      });
+    });
+
+    it('expect to be invalid if there are more than three values in the coordinates field', (done) => {
+      const locationData = {...initLocationData};
+      locationData.coordinates = { lat: 3.42424 }
+
+      const location = new Location(locationData);
+      location.validate((err) => {
+        expect(Object.keys(err.errors).length).to.be.equal(1);
+        expect(err.errors).to.include.keys('coordinates.long');
+        expect(err.errors['coordinates.long'].message).to.equal('Path `coordinates.long` is required.');
+        done();
+      });
+    });
+
+    it('expect to be invalid if the coordinates are out of the lat long ranges', (done) => {
+      const locationData = {...initLocationData};
+      locationData.coordinates = { lat: 5000, long: 5000 };
+
+      const location = new Location(locationData);
+      location.validate((err) => {
+        expect(Object.keys(err.errors).length).to.be.equal(2);
+        expect(err.errors).to.include.keys('coordinates.long');
+        expect(err.errors['coordinates.long'].message).to.equal('Longitude is not in the correct range');
+        expect(err.errors).to.include.keys('coordinates.lat');
+        expect(err.errors['coordinates.lat'].message).to.equal('Latitude is not in the correct range');
+        done();
+      });
+    });
+
+    it('expect truckNum to exist and be 1 after creation when not provided', (done) => {
+      const locationData = {...initLocationData};
+      delete locationData.truckNum;
+
+      const location = new Location(locationData);
+      location.validate((err) => {
+        expect(err).to.be.null;
+        expect(location.truckNum).to.be.equal(1);
+        done();
+      });
+    });
+  });
 
 //   describe('Comment Schema', () => {
 //     it('expect to be invalid if the comment length is under 5 characters long', (done) => {
