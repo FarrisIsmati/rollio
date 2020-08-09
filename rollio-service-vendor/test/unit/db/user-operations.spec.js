66f701bb2a4b28dce01b463104f676f1232a298c
farris.ismati@gmail.com <farris.ismati@gmail.com>
2020-08-09 17:40:24 -0400

    added 1 useroperation

---
+++ rollio-service-vendor/test/unit/db/user-operations.spec.js
@@ -0,0 +1,53 @@
+/* eslint-disable no-console */
+// DEPENDENCIES
+const chai = require('chai');
+const chaid = require('chaid');
+const dateTime = require('chai-datetime');
+const assertArrays = require('chai-arrays');
+const { ObjectId } = require('mongoose').Types;
+const mongoose = require('../../../lib/db/mongo/mongoose/index');
+const sinon = require('sinon');
+
+const { expect } = chai;
+
+// OPERATIONS
+const vendorOps = require('../../../lib/db/mongo/operations/vendor-ops');
+// const regionOps = require('../../../lib/db/mongo/operations/region-ops');
+// const tweetOps = require('../../../lib/db/mongo/operations/tweet-ops');
+// const userOps = require('../../../lib/db/mongo/operations/user-ops');
+const sharedOps = require('../../../lib/db/mongo/operations/shared-ops');
+const { error } = require('winston');
+const { ObjectID } = require('mongodb');
+const userOps = require('../../../lib/db/mongo/operations/user-ops');
+
+// SCHEMAS
+const Vendor = mongoose.model('Vendor');
+const Region = mongoose.model('Region');
+const Location = mongoose.model('Location');
+const Tweet = mongoose.model('Tweet');
+const User = mongoose.model('User');
+
+chai.use(chaid);
+chai.use(assertArrays);
+chai.use(dateTime);
+
+describe('User Operations', () => {
+    afterEach(() => {
+        // Restore sinon spies,stubs,mocks,etc.
+        sinon.restore();
+    });
+
+    const popualte3 = { populate: sinon.stub().returns(Promise.resolve({})) }
+    const populate2 = { populate: sinon.stub().returns(popualte3) }
+    const populate1 = { populate: sinon.stub().returns(populate2) }
+
+    it('expects findUserById to return \'result1\'', async () => {
+        const userFindByIdStub = sinon.stub(User, 'findById').returns({ select: sinon.stub().returns('result1') });
+        const userId = new ObjectId()
+
+        const result = await userOps.findUserById(userId, true);
+
+        sinon.assert.called(userFindByIdStub);
+        expect(result).to.be.equal('result1');
+    });
+});
