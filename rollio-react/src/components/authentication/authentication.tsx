8f0ce8bf095bcb83ebe2d95a3b21e4a44a45de77
farris.ismati@gmail.com <farris.ismati@gmail.com>
2020-09-22 14:00:40 -0400

    start work on login frame

--- rollio-react/src/components/authentication/authentication.tsx
+++ rollio-react/src/components/authentication/authentication.tsx
@@ -2,11 +2,17 @@
 import React from "react";
 import { withRouter } from 'react-router';
 
+// COMPONENTS
+import LoginFrame from './login-frame';
+
 const Authentication = (props:any) => {
     return (
-        <div className='authentication__wrapper'>
-            <h2 className="twitter_login__pointer">Vendor Login</h2>
-            <p>Login My Dude?</p>
+        <div className='authentication__wrapper flex__center'>
+            <div className='authentication__login flex__center_column'>
+                <h2>Vendors</h2>
+                <p>Vendor login/sign up via twitter</p>
+                <LoginFrame />
+            </div>
         </div>
     );
 }
